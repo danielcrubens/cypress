edb9fd9c6ffec1b2024bec3e5a6cf992eb3925f7
danielcrubens <dcr.tecnologi@gmail.com>
2023-03-11 22:23:49 -0300

    criando descrição  :white_check_mark:

---
+++ cypress/e2e/finances.cy.js
@@ -0,0 +1,16 @@
+describe('Transações',()=>{
+  it('Cadastrar uma entrada',()=>{
+    cy.visit("https://devfinance-agilizei.netlify.app/")
+
+    cy.contains("Nova Transação").click()
+    cy.get('#description').type('Freela')
+    cy.get('#amount').type(250)
+    cy.get('#date').type('2023-03-11')
+
+    cy.contains('button','Salvar').click()
+
+    cy.get('tbody tr td.description').should('have.text','Freela')
+  })
+
+  
+})
